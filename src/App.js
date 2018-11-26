fbed6003bfbdf9e1b50dd6cdf11f557fc0775bc2
Saloni Gupta <saloni08g@gmail.com>
2018-11-26 21:33:08 +0530

    Foursquare API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
-import Map from './Components/Map.js'
+import Map from './Components/Map'
+import SquareAPI from './API/'
 
 class App extends Component {
+
+  componentDidMount() {
+    SquareAPI.search({
+      near: "Bhopal, India",
+      query: "food",
+      limit: 10
+    }).then(results => console.log(results));
+  }
   render() {
     return (
       <div className="App">
--- src/Components/Map.js
+++ src/Components/Map.js
@@ -8,15 +8,15 @@ class Map extends Component {
     const MyMapComponent = withScriptjs(withGoogleMap((props) =>
   <GoogleMap
     defaultZoom={8}
-    defaultCenter={{ lat: -34.397, lng: 150.644 }}
+    defaultCenter={{ lat: 23.259933, lng: 77.412615 }}
   >
-    {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
+    {props.isMarkerShown && <Marker position={{ lat: 23.259933, lng: 77.412615 }} />}
   </GoogleMap>
 ))
     return (
       <MyMapComponent
         isMarkerShown
-        googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyDIH856EDhM6HZ9pI3A5NhPko9vjNOwBhw&v=3&callback=initMap"
+        googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyDIH856EDhM6HZ9pI3A5NhPko9vjNOwBhw&v=3"
         loadingElement={<div style={{ height: `100%` }} />}
         containerElement={<div style={{ height: `400px` }} />}
         mapElement={<div style={{ height: `100%` }} />}
